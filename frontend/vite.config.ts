ab5acb85d9c68206c9abc17f6f79f2254a8b5d74
christophersiem <christopher.siem@gmail.com>
2025-10-30 12:16:02 +0100

    Add remote host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      'drecz-2003-f8-3f1c-400-ecf1-3436-379d-ef4.a.free.pinggy.link',
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
